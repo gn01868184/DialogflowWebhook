e1566374101476e9c97811dc039f9e1a2a7d7986
gn01868184 <gn01868184@yahoo.com.tw>
2020-06-16 13:44:30 +0800

    Create SessionRequest.java

---
+++ src/main/java/springboot/webhook/mongodb/entity/SessionRequest.java
@@ -0,0 +1,15 @@
+package springboot.webhook.mongodb.entity;
+
+public class SessionRequest {
+
+	private String sessionId;
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	
+}
