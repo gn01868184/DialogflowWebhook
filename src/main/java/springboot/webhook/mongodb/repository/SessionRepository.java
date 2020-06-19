1e9081c83f795d111855c7da3f87aaba8036e554
gn01868184 <gn01868184@yahoo.com.tw>
2020-06-19 15:29:43 +0800

    Update SessionRepository.java

--- src/main/java/springboot/webhook/mongodb/repository/SessionRepository.java
+++ src/main/java/springboot/webhook/mongodb/repository/SessionRepository.java
@@ -1,11 +1,10 @@
 package springboot.webhook.mongodb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import springboot.webhook.mongodb.entity.Session;
 
-@Repository
 public interface SessionRepository extends MongoRepository<Session, String> {
 	public Session findBySessionId(String sessionId);   
+	public Session deleteBySessionId(String sessionId);   
 }
